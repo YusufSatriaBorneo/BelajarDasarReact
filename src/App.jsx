677b13c741bb786231c74c20e9b5a4b3ab9c28bc
YusufSatriaBorneo <yusufsatriaborneo@gmail.com>
2023-11-22 15:32:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+import Dashboard from "./componen/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <Dashboard name="Yusuf" />
+      <Dashboard name="Satria" />
+      <Dashboard name="Borneo" />
+    </div>
+  );
+}
+
+export default App;
